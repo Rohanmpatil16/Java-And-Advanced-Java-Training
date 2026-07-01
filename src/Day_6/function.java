8718f4645dc1f03994276b8d47595e50d2a797cf
Rohanmpatil16 <rohanmpatil18@gmail.com>
2026-07-01 11:23:09 +0530

    reverse string using recursion

--- src/Day_6/function.java
+++ src/Day_6/function.java
@@ -3,20 +3,19 @@ package Day_6;
 public class function {
 
 	public static void main(String[] args) {
-		int i=0;
-		int n=5;
+		String s="Rohan";
+		int i=s.length()-1;
+		int j=0;
 		
-		fun(i,n);
+		fun(i,j,s);
 		
 	}
 	
-	public static void fun(int i,int n)
+	public static void fun(int i,int j,String s)
 	{
-		if(i==n) return;
-		System.out.println("Rohan");
-		
-		fun(i+1,n);
-		System.out.println("Patil");
+		if(i<0) return;
+		System.out.print(s.charAt(i));
+		fun(i-1,j,s);
 
 	}
 
